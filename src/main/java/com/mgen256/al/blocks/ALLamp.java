cc1e9ca1b20e63f51586960efbeea1a36c1093c3
Johnmgen256 <mgen256@gmail.com>
2020-04-19 09:41:47 +0900

    1

--- src/main/java/com/mgen256/al/blocks/ALLamp.java
+++ src/main/java/com/mgen256/al/blocks/ALLamp.java
@@ -27,15 +27,15 @@ import com.mgen256.al.AdditionalLights;
 
 public class ALLamp extends ModBlock implements IWaterLoggable{
 
+    // D-U-N-S-W-E
     private static VoxelShape[] SHAPES = {
-        // D-U-N-S-W-E
-        Block.makeCuboidShape( 6.0, 15.0, 6.0, 10.0, 16.0, 10.0), // down
-        Block.makeCuboidShape( 6.0, 0.0, 6.0, 10.0, 1.0, 10.0), // up
+        Block.makeCuboidShape( 5.0, 15.0, 5.0, 11.0, 16.0, 11.0), // down
+        Block.makeCuboidShape( 5.0, 0.0, 5.0, 11.0, 1.0, 11.0), // up
 
-        Block.makeCuboidShape(7.0, 8.0, 13.0, 9.0, 12.0, 16.0), // north
-        Block.makeCuboidShape(7.0, 8.0, 0.0, 9.0, 12.0, 3.0), // south
-        Block.makeCuboidShape(13.0, 8.0, 7.0, 16.0, 12.0, 9.0), // west
-        Block.makeCuboidShape(0.0, 8.0, 7.0, 3.0, 12.0, 9.0), // east
+        Block.makeCuboidShape(6.0, 7.0, 12.0, 10.0, 13.0, 16.0), // north
+        Block.makeCuboidShape(6.0, 7.0, 0.0, 10.0, 13.0, 4.0), // south
+        Block.makeCuboidShape(12.0, 7.0, 6.0, 16.0, 13.0, 10.0), // west
+        Block.makeCuboidShape(0.0, 7.0, 6.0, 4.0, 13.0, 10.0), // east
     }; 
 
 
